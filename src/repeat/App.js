7e28cc4f647b912f448b76431b0b9b0b74fbb4b2
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-01-24 00:32:55 +0900

    복습 - useRef, useState, 컴포넌트 map

--- src/repeat/App.js
+++ src/repeat/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: '예진',
+    content: '하이 1',
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: '예진2',
+    content: '하이 2',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '예진3',
+    content: '하이 3',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
--- src/repeat/DiaryEditor.js
+++ src/repeat/DiaryEditor.js
@@ -1,13 +1,74 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const DiaryEditor = () => {
-  const [author, setAuthor] = useState('');
+  const authorInput = useRef();
+  const contentInput = useRef();
+
+  // state를 하나로 관리할 수 있다.
+  // setState시 새로운 객체를 만들어 전달해줘야 함
+  // 스프레드 연산자를 사용하면 쉽게 사용할 수 있다. => 변화되는 값만 재명시
+  const [state, setState] = useState({
+    author: '',
+    content: '',
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
+    alert('저장 성공!');
+  };
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <input
+          ref={authorInput}
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <span>오늘의 감정 점수 : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
